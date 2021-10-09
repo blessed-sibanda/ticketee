e4298eadbde56bb2443c0e6de05566920d1b2f9a
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-10-09 16:36:48 +0200

    Add ability to create users through the admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get "users/index"
+  end
   resources :projects, only: [:index, :show] do
     resources :tickets
   end
   namespace :admin do
     root "application#index"
     resources :projects, except: %i[index show]
+    resources :users
   end
   devise_for :users
   root "projects#index"
