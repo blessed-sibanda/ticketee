282a1edc030fb6cb7b8ba1f641d1620683d53bcb
Blessed Sibanda <blessedsibanda.me@gmail.com>
2021-10-09 08:26:52 +0200

    Edit projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,6 +7,20 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to @project, notice: "Project has been updated."
+    else
+      flash.now[:alert] = "Project has not been updated."
+      render :edit
+    end
+  end
+
   def create
     @project = Project.new(project_params)
     if @project.save
